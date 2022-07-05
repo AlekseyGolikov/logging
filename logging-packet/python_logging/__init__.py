521fc1f6c09b6589d1c9fd0891e03a755edba8ed
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-07-05 12:31:56 +0300

    update logging_packets - Настройки журналирования импортируются из кофигурационного json-файла

--- logging-packet/python_logging/__init__.py
+++ logging-packet/python_logging/__init__.py
@@ -2,21 +2,28 @@
 from os import path, remove
 import logging
 import logging.config
+import json
 
-from .first_class import FirstClass
-from .second_class import SecondClass
 if path.isfile('python_logging.log'):
     remove('python_logging.log')
 
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+#
+# logger_handler = logging.FileHandler('python_logging.log')
+# logger_handler.setLevel(logging.INFO)
+#
+# logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+#
+# logger_handler.setFormatter(logger_formatter)
+#
+# logger.addHandler(logger_handler)
+# logger.info('Настройка логгирования окончена!')
+
+with open('python_logging/logcnf.json', 'r') as logCnf:
+    config_dict = json.load(logCnf)
+
+logging.config.dictConfig(config_dict)
+
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-logger_handler = logging.FileHandler('python_logging.log')
-logger_handler.setLevel(logging.INFO)
-
-logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-
-logger_handler.setFormatter(logger_formatter)
-
-logger.addHandler(logger_handler)
 logger.info('Настройка логгирования окончена!')
